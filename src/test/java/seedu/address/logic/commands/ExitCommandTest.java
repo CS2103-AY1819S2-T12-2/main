e2c42b3deebe69ebfa1ed09431272a577c256346
Sergio Vieri <sergio.vieri.hp@gmail.com>
2019-04-01 17:24:34 +0800

    Add SmartCommandTest and more ExitCommandTest

--- src/test/java/seedu/address/logic/commands/ExitCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ExitCommandTest.java
@@ -1,13 +1,17 @@
 package seedu.address.logic.commands;
 
+import static org.junit.Assert.assertEquals;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.ExitCommand.MESSAGE_EXIT_ACKNOWLEDGEMENT;
+import static seedu.address.testutil.TypicalFlashcards.getTypicalCardCollection;
 
 import org.junit.Test;
 
 import seedu.address.logic.CommandHistory;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
 
 public class ExitCommandTest {
     private Model model = new ModelManager();
@@ -18,5 +22,13 @@ public class ExitCommandTest {
     public void execute_exit_success() {
         CommandResult expectedCommandResult = new CommandResult(MESSAGE_EXIT_ACKNOWLEDGEMENT, false, true);
         assertCommandSuccess(new ExitCommand(), model, commandHistory, expectedCommandResult, expectedModel);
+    }
+
+    @Test
+    public void execute_inQuiz_exitsQuiz() throws CommandException {
+        Model newModel = new ModelManager(getTypicalCardCollection(), new UserPrefs());
+        new QuizCommand().execute(newModel, commandHistory);
+        new ExitCommand().execute(newModel, commandHistory);
+        assertEquals(0, (int) newModel.getQuizMode());
     }
 }
---
+++ src/test/java/seedu/address/logic/commands/SmartCommandTest.java
@@ -0,0 +1,37 @@
+package seedu.address.logic.commands;
+
+import static org.junit.Assert.assertEquals;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.testutil.TypicalFlashcards.getTypicalCardCollection;
+
+import org.junit.Test;
+
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+
+/**
+ * Contains integration tests and unit tests for SmartCommand.
+ */
+public class SmartCommandTest {
+
+    private Model model = new ModelManager(getTypicalCardCollection(), new UserPrefs());
+    private CommandHistory commandHistory = new CommandHistory();
+
+    @Test
+    public void execute_inQuiz_success() throws CommandException {
+        new QuizCommand().execute(model, commandHistory);
+        new SmartCommand().execute(model, commandHistory);
+        assertEquals((int) model.getQuizMode(), 1);
+        new SmartCommand().execute(model, commandHistory);
+        assertEquals((int) model.getQuizMode(), -1);
+    }
+
+    @Test
+    public void execute_notInQuiz_failure() throws CommandException {
+        assertCommandFailure(new ShowCommand(), model, commandHistory, ShowCommand.MESSAGE_FAILURE_NOT_QUIZ_MODE);
+    }
+
+}
