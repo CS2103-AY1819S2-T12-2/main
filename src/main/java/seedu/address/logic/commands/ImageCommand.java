10b60d61d54f9752189c71adc920185932d8c26d
Robin Christopher Yu <robinccyu@gmail.com>
2019-04-03 16:13:02 +0800

    Add tests for ImageCommand

--- src/main/java/seedu/address/logic/commands/ImageCommand.java
+++ src/main/java/seedu/address/logic/commands/ImageCommand.java
@@ -31,7 +31,7 @@ public class ImageCommand extends Command {
     public static final String MESSAGE_SUCCESS = "Image %s successfully imported into: %s.";
     public static final String MESSAGE_INVALID_FILE = "Image %s not found!";
     public static final String MESSAGE_DUPLICATE_NAME = "Image with filename %s already exists in working directory.";
-    public static final String MESSAGE_IMPORT_FAIL = "Failed to import the image. Please check your permission"
+    public static final String MESSAGE_IMPORT_FAIL = "Failed to import the image. Please check your permissions."
         + "settings.";
 
     private final File toImport;
@@ -56,30 +56,23 @@ public class ImageCommand extends Command {
         }
 
         //make a copy of the image
-        InputStream inputStream = null;
-        OutputStream outputStream = null;
         try {
-            inputStream = new FileInputStream(toImport);
-            outputStream = new FileOutputStream(workingDirectoryFile);
+            InputStream inputStream = new FileInputStream(toImport);
+            OutputStream outputStream = new FileOutputStream(workingDirectoryFile);
             byte[] buffer = new byte[1024];
             int length;
             while ((length = inputStream.read(buffer)) > 0) {
                 outputStream.write(buffer, 0, length);
             }
             outputStream.flush();
+            inputStream.close();
+            outputStream.close();
         } catch (FileNotFoundException e) {
             //ImageCommandParser should have already made this check;
             //so this should not be possible. But use redundancy for defensive coding
             throw new CommandException(String.format(MESSAGE_INVALID_FILE, toImport));
-        } catch (IOException e) {
+        } catch (IOException | NullPointerException e) {
             throw new CommandException(MESSAGE_IMPORT_FAIL);
-        } finally {
-            try {
-                inputStream.close();
-                outputStream.close();
-            } catch (IOException e) {
-                // ignore
-            }
         }
         return new CommandResult(String.format(MESSAGE_SUCCESS, toImport, workingDirectoryFile));
     }
