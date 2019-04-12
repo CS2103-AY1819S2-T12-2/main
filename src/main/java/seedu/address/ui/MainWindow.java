e0e087de9c3217dc55baacc274b9daaa65b04198
lungsin <kweelungsin@gmail.com>
2019-04-12 16:46:32 +0800

    UI: add is review mode in the UI

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -185,7 +185,7 @@ public class MainWindow extends UiPart<Stage> {
     private void startQuizMode() {
         flashcardListPanelPlaceholder.getChildren().clear();
         QuizPanel quizPanel = new QuizPanel(logic.getQuizFlashcards(),
-            logic.quizGoodProperty(), logic.quizBadProperty());
+            logic.quizGoodProperty(), logic.quizBadProperty(), logic.isQuizSRSProperty());
         flashcardListPanelPlaceholder.getChildren().add(quizPanel.getRoot());
     }
 
