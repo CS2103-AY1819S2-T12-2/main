fda0bed0040bbd2c55153c41524c0fcd84c65112
lucatriglia <lucatriglia@gmail.com>
2019-04-13 18:41:13 +0800

    Add tests for Share Command

--- src/test/java/seedu/address/logic/commands/ShareCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ShareCommandTest.java
@@ -1,15 +1,18 @@
 package seedu.address.logic.commands;
 
+import static org.junit.Assert.assertEquals;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_PATH;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.testutil.TypicalFlashcards.getTypicalCardCollection;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.File;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.logic.parser.ShareCommandParser;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
@@ -26,39 +29,19 @@ public class ShareCommandTest {
     }
 
     @Test
-    public void execute_share_success() {
-        Path testDataFolder = Paths.get("src", "test", "data", "uploadCommandTest");
-        String file = testDataFolder.resolve("flashcardsCorrupted.txt").toString();
-
-        try {
-            new ShareCommand(testDataFolder.toString()).execute(model, commandHistory);
-        } catch (CommandException e) {
-            e.printStackTrace();
-        }
+    public void execute_share_success() throws CommandException {
+        File expectedFile = new File("src\\test\\data\\shareCommandTestFiles\\flashcardsExpected.txt");
+        ShareCommand share = new ShareCommand("src\\test\\data\\shareCommandTestFiles\\");
+        share.execute(model, commandHistory);
+        File outputFile = new File("src\\test\\data\\shareCommandTestFiles\\flashcards.txt");
+        assertEquals(expectedFile.length(), outputFile.length());
     }
 
-
-
-
-/*
-    //This test fails on Travis for some reason
-    //It works locally
-    @Ignore
     @Test
-    public void execute_imageCommand_success() throws CommandException {
-        File imageToProduce = new File(IMAGE_DIRECTORY.concat(validFlashcard));
-        File imageToTest = new File("src\\test\\data\\images\\".concat(validFlashcard));
-        if (!imageToTest.exists()) {
-            logger.severe("Image ".concat(imageToTest.getAbsolutePath()).concat(" couldn't be found."));
-        }
-        if (imageToProduce.delete()) {
-            logger.warning("Image ".concat(imageToProduce.getAbsolutePath()).concat(" deleted."));
-        }
-        ImageCommand command = new ImageCommand(imageToTest);
-        command.execute(model, commandHistory);
-        assert(imageToProduce.exists());
-        if (!imageToProduce.delete()) {
-            logger.severe("Image ".concat(imageToProduce.getAbsolutePath()).concat(" couldn't be deleted."));
-        }
-    }*/
+    public void parse_invalidDirectory_failure() {
+        ShareCommandParser parser = new ShareCommandParser();
+        assertParseFailure(parser, "src\\test\\data\\importCommandTestFiles\\hello.txt",
+                String.format(MESSAGE_INVALID_PATH, ShareCommand.MESSAGE_USAGE));
+    }
+
 }
